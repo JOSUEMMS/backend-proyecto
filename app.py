0c80fb42c694e2ebf6e826f78b10b5007bbe8ea9
Josue Muñoz <39468357+JOSUEMMS@users.noreply.github.com>
2025-09-26 21:31:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1304,4 +1304,4 @@ if __name__ == '__main__':
         os.makedirs('facturas')
     import os
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port) 
+    app.run(host="0.0.0.0", port=port)
